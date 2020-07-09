5c1806890a9f26adecfb8b9234635294edaf69c5
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-07-10 01:40:05 +0300

    Last touches

--- Fundamentals exam with Security/src/main/java/exam/security/web/HomeController.java
+++ Fundamentals exam with Security/src/main/java/exam/security/web/HomeController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
 
+import java.security.Principal;
+
 @Controller
 public class HomeController {
 
@@ -16,23 +18,31 @@ public class HomeController {
     }
 
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
+//    @GetMapping("/")
+//    public String index() {
+//        return "index";
+//    }
 
     @GetMapping("/home")
-    public String home(Model model) {
+    public String home(Model model, Principal principal) {
 
+        if (principal.getName() != null) {
         model.addAttribute("products", this.productService.findAllProducts());
         model.addAttribute("price", this.productService.getTotalPrice());
-
         return "home";
+        } else {
+            return "index";
+        }
     }
 
     @GetMapping("/unauthorized")
     public String unauthorized() {
         return "unauthorized";
+    }
+
+    @GetMapping("/login")
+    public String loginRedirect() {
+        return "redirect:/users/login";
     }
 
 }
